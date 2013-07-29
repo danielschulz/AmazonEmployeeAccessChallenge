c18dc2e1e89ef793d9cc769aaf31669f9bc9447d
Daniel Schulz <danielschulz2005@hotmail.com>
2013-07-29 20:16:07 +0200

    classification

--- AEAC/classification.R
+++ AEAC/classification.R
@@ -11,15 +11,19 @@ library(kernlab)
 # clean
 rm(list = ls()[!(ls() %in% PERSISTENT_CONSTANTS)])
 
+setwd("C:/Users/schulzda/Desktop/Work/AEAC/AEAC/AEAC/")
+
 # constants
 PERSISTENT_CONSTANTS = c("PERSISTENT_CONSTANTS", "WD")
 WD = getwd()
 
 dataLocation = paste0(WD, "/input/data/train.csv")
+dataLocationTargets = paste0(WD, "/input/data/test.csv")
 
 
 # LOAD DATA
 rawData = read.csv(dataLocation, header=TRUE, sep=",")
+rawDataTargets = read.csv(dataLocationTargets, header=TRUE, sep=",")
 
 
 # TRANSFORM
@@ -35,14 +39,32 @@ rawData$ROLE_FAMILY = as.numeric(rawData$ROLE_FAMILY)
 rawData$ROLE_CODE = as.numeric(rawData$ROLE_CODE)
 
 
-data = rawData
+rawDataTargets$id = as.numeric(rawDataTargets$id)
+rawDataTargets$ACTION = as.numeric(0)
+rawDataTargets$RESOURCE = as.numeric(rawDataTargets$RESOURCE)
+rawDataTargets$MGR_ID = as.numeric(rawDataTargets$MGR_ID)
+rawDataTargets$ROLE_ROLLUP_1 = as.numeric(rawDataTargets$ROLE_ROLLUP_1)
+rawDataTargets$ROLE_ROLLUP_2 = as.numeric(rawDataTargets$ROLE_ROLLUP_2)
+rawDataTargets$ROLE_DEPTNAME = as.numeric(rawDataTargets$ROLE_DEPTNAME)
+rawDataTargets$ROLE_TITLE = as.numeric(rawDataTargets$ROLE_TITLE)
+rawDataTargets$ROLE_FAMILY_DESC = as.numeric(rawDataTargets$ROLE_FAMILY_DESC)
+rawDataTargets$ROLE_FAMILY = as.numeric(rawDataTargets$ROLE_FAMILY)
+rawDataTargets$ROLE_CODE = as.numeric(rawDataTargets$ROLE_CODE)
 
+
+data = rawData
+targets = rawDataTargets
+
+rm(list=c("rawData", "rawDataTargets", "dataLocation", "dataLocationTargets"))
 
 # CLASSIFICATION
-testDataIndex = c(rep(FALSE, nrow(data)))
-d = rbind(data)
+data$id = -1
+trainTrainDataSize = floor(nrow(data)/100)
+trainIgnoreDataSize = nrow(data) - trainTrainDataSize
+testDataIndex = c(rep(FALSE, trainTrainDataSize), rep(TRUE, (nrow(targets) + trainIgnoreDataSize)))
+d = rbind(data, targets)
 
-formula = data$ACTION ~ .
+formula = data$ACTION ~ data$RESOURCE + data$MGR_ID + data$ROLE_ROLLUP_1 + data$ROLE_ROLLUP_2 + data$ROLE_DEPTNAME + data$ROLE_TITLE + data$ROLE_FAMILY_DESC + data$ROLE_FAMILY + data$ROLE_CODE
 svm = ksvm(formula, data = d[!testDataIndex,], kernel="rbfdot", kpar=list(sigma=0.015), C=70, cross=4, prob.model=TRUE, na.action=na.roughfix)
 pr = predict(svm, newdata = d[testDataIndex,])
 
